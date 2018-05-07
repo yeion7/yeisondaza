d6931c6c9b99a6218c36ac9b1b79ab9838410930
Yeison Daza <jahir045@gmail.com>
2018-05-07 06:44:13 -0500

    '#add: typography'

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,13 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import doelgerTheme from 'typography-theme-doelger'
 
-Wordpress2016.overrideThemeStyles = () => ({
+doelgerTheme.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
 })
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(doelgerTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
