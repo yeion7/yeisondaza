a17064d2dff36f5a90014e1ae4376ab6acaecb2b
Yeison Daza <jahir045@gmail.com>
2018-05-30 21:38:22 -0500

    fix: styles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,4 +3,5 @@ import 'netlify-cms/dist/cms.css'
 
 import PostPreview from './preview-templates/blogPreview'
 
+CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('blog', PostPreview)
